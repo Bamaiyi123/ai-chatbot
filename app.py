1d6e8fcfe10f1f8a041847f9c4944a4342c60fec
Bamaiyi123 <adeyibamaiyi@gmail.com>
2026-03-27 14:35:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,8 @@ def index():
     return app.send_static_file('index.html')
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
